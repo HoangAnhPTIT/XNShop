3117fad33a8982ea49c48a374f71555b0b8d0841
anhch <anhch@tuivietnam.com>
2021-08-01 16:05:51 +0700

    remove redundant code

--- util/connectDb.js
+++ util/connectDb.js
@@ -6,12 +6,6 @@ const sequelize = new Sequelize('XNShop', 'admin', 'password', {
     max: 5,
     min: 0,
     idle: 10000
-  },
-  dialectOptions: {
-    socketPath: '/var/run/mysqld/mysqld.sock'
-  },
-  define: {
-    paranoid: true
   }
 })
 
