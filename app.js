96c95a0ebd7e094a989abcf48d0fbdf81dec3c29
duongnt <duongnt@tuivietnam.com>
2021-08-03 16:59:13 +0700

    add image to highlightproducts

--- app.js
+++ app.js
@@ -1,38 +1,38 @@
-const express = require('express')
-const cors = require('cors')
-const app = express()
-const { synchronizingModel } = require('./util/connectDb')
-const routers = require('./router/index')
-const resolveJwtToken = require('./middleware/jwt')
+const express = require("express");
+const cors = require("cors");
+const app = express();
+const { synchronizingModel } = require("./util/connectDb");
+const routers = require("./router/index");
+const resolveJwtToken = require("./middleware/jwt");
 
-app.use(cors())
-app.use(express.json())
+app.use(cors());
+app.use(express.json());
 app.use(
   express.urlencoded({
-    extended: true
+    extended: true,
   })
-)
+);
 
-app.use(resolveJwtToken)
+app.use(resolveJwtToken);
 
-const port = 8000
+const port = 8000;
 
-synchronizingModel()
-  .then(() => {
-    console.log('Create table !!!')
-  })
-  .catch((err) => {
-    console.log(err)
-  })
+// synchronizingModel()
+//   .then(() => {
+//     console.log('Create table !!!')
+//   })
+//   .catch((err) => {
+//     console.log(err)
+//   })
 
-app.post('/ping', async (req, res) => {
-  res.json({ message: 'Pinggg' })
-})
+app.post("/ping", async (req, res) => {
+  res.json({ message: "Pinggg" });
+});
 
 routers.forEach((router) => {
-  app.use('/api/v1', router)
-})
+  app.use("/api/v1", router);
+});
 
 app.listen(process.env.PORT || 8000, () => {
-  console.log(`App listening at http://localhost:${port}`)
-})
+  console.log(`App listening at http://localhost:${port}`);
+});
--- controller/homepageController.js
+++ controller/homepageController.js
@@ -1,70 +1,92 @@
-const { Banners, Products, Categories, ChildTypes, Images } = require('../model')
-const { Op } = require('sequelize')
-async function getGiftProduct () {
+const {
+  Banners,
+  Products,
+  Categories,
+  ChildTypes,
+  Images,
+} = require("../model");
+const { Op } = require("sequelize");
+async function getGiftProduct() {
   const giftProduct = await Products.findAll({
     where: {
-      type: 'GIFT'
-    }
-  })
-  return giftProduct
+      type: "GIFT",
+    },
+  });
+  return giftProduct;
 }
 
-async function getProductByChildType (categoryCode) {
+async function getProductByChildType(categoryCode) {
   const category = await Categories.findOne({
     where: {
-      code: categoryCode
-    }
-  })
-  const productData = {}
-  const listChildType = await category.getChildTypes()
-  const listNameChildType = listChildType.map(x => x.name)
-  const listProduct = {}
+      code: categoryCode,
+    },
+  });
+  const productData = {};
+  const listChildType = await category.getChildTypes();
+  const listNameChildType = listChildType.map((x) => x.name);
+  const listProduct = {};
   for (const childType of listChildType) {
-    const name = childType.name
+    const name = childType.name;
     const products = await Products.findAll({
       where: {
-        type: { [Op.ne]: 'GIFT' }
+        type: { [Op.ne]: "GIFT" },
       },
       include: [
         {
           model: ChildTypes,
           where: { id: childType.id },
-          attributes: []
+          attributes: [],
         },
         {
           model: Images,
-          attributes: ['url']
-        }
-      ]
-    })
-    listProduct[name] = products
+          attributes: ["url"],
+        },
+      ],
+    });
+    listProduct[name] = products;
   }
-  productData.listNameChildType = listNameChildType
-  productData.listProduct = listProduct
-  return productData
+  productData.listNameChildType = listNameChildType;
+  productData.listProduct = listProduct;
+  return productData;
 }
 
-async function getHightLightProduct () {
+async function getHighLightProducts() {
   const hightLightProduct = await Products.findAll({
-    order: [['view', 'DESC']],
-    limit: 6
-  })
-  return hightLightProduct
+    include: [
+      {
+        model: ChildTypes,
+        attributes: [],
+      },
+      {
+        model: Images,
+        attributes: ["url"],
+      },
+    ],
+    order: [["view", "DESC"]],
+    limit: 6,
+  });
+  return hightLightProduct;
 }
 
-async function getDataHomepage (req, res) {
+async function getDataHomepage(req, res) {
   try {
-    const banners = await Banners.findAll({ order: ['position'], limit: 4 })
-    const giftProduct = await getGiftProduct()
-    const listClockProduct = await getProductByChildType('CLOCK')
-    const listLightProduct = await getProductByChildType('LIGHT')
-    const hightLightProduct = await getHightLightProduct()
-    res.json({ banners, giftProduct, listClockProduct, listLightProduct, hightLightProduct })
+    const banners = await Banners.findAll({ order: ["position"], limit: 4 });
+    const giftProduct = await getGiftProduct();
+    const listClockProduct = await getProductByChildType("CLOCK");
+    const listLightProduct = await getProductByChildType("LIGHT");
+    const highLightProducts = await getHighLightProducts();
+    res.json({
+      banners,
+      giftProduct,
+      listClockProduct,
+      listLightProduct,
+      highLightProducts,
+    });
   } catch (error) {
-    res.status(422).json(error)
+    res.status(422).json(error.message);
   }
 }
 
 module.exports = {
-  getDataHomepage
-}
+  getDataHomepage,
+};
