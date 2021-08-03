d5bcde2ed984d2e1bad648b59ba0aca7e6492bbd
anhch <anhch@tuivietnam.com>
2021-08-03 15:39:51 +0700

    change db

--- util/connectDb.js
+++ util/connectDb.js
@@ -1,10 +1,19 @@
 const { Sequelize } = require('sequelize')
 require('dotenv').config()
-// const username = process.env.USERNAMEDB
-// const database = process.env.DATABASE
-// const password = process.env.PASSWORD
-// const host = process.env.HOST
-const sequelize = new Sequelize(process.env.URL_CONNECT_DB)
+const username = process.env.USERNAMEDB
+const database = process.env.DATABASE
+const password = process.env.PASSWORD
+const host = process.env.HOST
+const sequelize = new Sequelize(database, username, password, {
+  host: host,
+  dialect: 'mysql',
+  pool: {
+    max: 5,
+    min: 0,
+    idle: 10000
+  },
+  ssl: true
+})
 
 async function synchronizingModel () {
   await sequelize.sync({ alter: true })
