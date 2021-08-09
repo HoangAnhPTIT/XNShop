1cec85f3c354c361e0d308624bfaaf23392b19d4
Cao Hoàng Anh <57166038+HoangAnhPTIT@users.noreply.github.com>
2021-08-09 21:45:11 +0700

    api update product (#3)
    Co-authored-by: anhch <anhch@tuivietnam.com>

--- router/productRouter.js
+++ router/productRouter.js
@@ -1,11 +1,13 @@
 const express = require('express')
 const productRouter = express.Router()
-const { create, index, getProductByCollection, uploadImages } = require('../controller/productController')
+const { create, index, getProductByCollection, uploadImages, findOne, update } = require('../controller/productController')
 const upload = require('../util/multer')
 
 productRouter.post('/product/create', create)
 productRouter.get('/products', index)
 productRouter.get('/products/collection', getProductByCollection)
 productRouter.post('/product/images', upload.array('images', 10), uploadImages)
+productRouter.get('/product/:id', findOne)
+productRouter.patch('/product/:id', update)
 
 module.exports = productRouter
