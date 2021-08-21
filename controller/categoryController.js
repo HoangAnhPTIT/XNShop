75de695802f2ae8f3f44582546ce0d9670ada1a6
anhch <anhch@tuivietnam.com>
2021-08-21 20:44:14 +0700

    api for category

--- controller/categoryController.js
+++ controller/categoryController.js
@@ -14,12 +14,12 @@ async function update (req, res) {
   const cateroryId = req.params.id
   const category = req.body.category
   try {
-    const categoryUpdate = await Categories.update(category, {
+    await Categories.update(category, {
       where: {
         id: cateroryId
       }
     })
-    res.json(categoryUpdate)
+    res.json({ status: 200, message: 'Success' })
   } catch (error) {
     res.status(422).json(error.message)
   }
@@ -38,8 +38,38 @@ async function index (req, res) {
   }
 }
 
+async function findOne (req, res) {
+  try {
+    const id = req.params.id
+    const category = await Categories.findByPk(id, {
+      include: {
+        model: ChildTypes
+      }
+    })
+    res.json({ status: 200, category })
+  } catch (error) {
+    res.status(422).json(error.message)
+  }
+}
+
+async function remove (req, res) {
+  try {
+    const ids = req.body.ids
+    await Categories.destroy({
+      where: {
+        id: ids
+      }
+    })
+    res.json({ status: 200, message: 'Success' })
+  } catch (error) {
+    res.status(422).json(error.message)
+  }
+}
+
 module.exports = {
   create,
   update,
-  index
+  index,
+  findOne,
+  remove
 }
--- router/categoryRouter.js
+++ router/categoryRouter.js
@@ -1,9 +1,10 @@
 const express = require('express')
 const categoryRouter = express.Router()
-const { create, update, index } = require('../controller/categoryController')
+const { create, update, index, remove, findOne } = require('../controller/categoryController')
 
-categoryRouter.post('/category/create', create)
-categoryRouter.patch('/category/:id', update)
+categoryRouter.post('/categories/create', create)
+categoryRouter.patch('/categories/:id', update)
 categoryRouter.get('/categories', index)
-
+categoryRouter.get('/categories/:id', findOne)
+categoryRouter.delete('/categories', remove)
 module.exports = categoryRouter
