45e5c8953ada22895dd392ff4983cb274a64a349
anhch <anhch@tuivietnam.com>
2021-08-24 23:25:13 +0700

    api for child type

--- controller/childTypeController.js
+++ controller/childTypeController.js
@@ -1,5 +1,14 @@
 const { ChildTypes } = require('../model')
 
+async function index (req, res) {
+  try {
+    const childTypes = await ChildTypes.findAll()
+    res.json(childTypes)
+  } catch (error) {
+    res.status(422).json(error)
+  }
+}
+
 async function create (req, res) {
   try {
     const childType = req.body.childType
@@ -10,6 +19,47 @@ async function create (req, res) {
   }
 }
 
+async function findOne (req, res) {
+  try {
+    const id = req.params.id
+    const childType = await ChildTypes.findByPk(id)
+    res.json({ status: 200, childType })
+  } catch (error) {
+    res.status(422).json(error)
+  }
+}
+
+async function update (req, res) {
+  try {
+    const childType = req.body.childType
+    const id = req.params.id
+    await ChildTypes.update(childType, {
+      where: { id }
+    })
+    res.json({ status: 200, message: 'Success' })
+  } catch (error) {
+    res.status(422).json(error)
+  }
+}
+
+async function remove (req, res) {
+  try {
+    const ids = req.body.ids
+    await ChildTypes.destroy({
+      where: {
+        id: ids
+      }
+    })
+    res.json({ status: 200, message: 'Success' })
+  } catch (error) {
+    res.status(422).json(error)
+  }
+}
+
 module.exports = {
-  create
+  index,
+  create,
+  findOne,
+  update,
+  remove
 }
--- router/childTypeRouter.js
+++ router/childTypeRouter.js
@@ -1,7 +1,11 @@
 const express = require('express')
 const childTypeRouter = express.Router()
-const { create } = require('../controller/childTypeController')
+const { create, index, findOne, remove, update } = require('../controller/childTypeController')
 
-childTypeRouter.post('/child-type/create', create)
+childTypeRouter.post('/child-types/create', create)
+childTypeRouter.get('/child-types', index)
+childTypeRouter.get('/child-types/:id', findOne)
+childTypeRouter.patch('/child-types/:id', update)
+childTypeRouter.delete('/child-types', remove)
 
 module.exports = childTypeRouter
