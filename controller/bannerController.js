240f632c07125032ef625f6db2eb0f78781c3d27
anhch <anhch@tuivietnam.com>
2021-08-29 16:04:02 +0700

    api for banners

--- controller/bannerController.js
+++ controller/bannerController.js
@@ -6,7 +6,7 @@ async function create (req, res) {
     const bannerRes = await Banners.create(banner)
     res.json(bannerRes)
   } catch (error) {
-    res.status(422).json(error)
+    res.status(422).json(error.message)
   }
 }
 
@@ -15,11 +15,51 @@ async function index (req, res) {
     const banners = await Banners.findAll({ order: ['position'], limit: 4 })
     res.json(banners)
   } catch (error) {
-    res.status(422).json(error)
+    res.status(422).json(error.message)
+  }
+}
+
+async function findOne (req, res) {
+  try {
+    const id = req.params.id
+    const banner = await Banners.findByPk(id)
+    res.json(banner)
+  } catch (error) {
+    res.status(422).json(error.message)
+  }
+}
+
+async function update (req, res) {
+  try {
+    const id = req.params.id
+    const banner = req.body.banner
+    await Banners.update(banner, {
+      where: { id }
+    })
+    res.json({ massage: 'Update banner success !!!' })
+  } catch (error) {
+    res.status(422).json(error.message)
+  }
+}
+
+async function remove (req, res) {
+  try {
+    const ids = req.body.ids
+    await Banners.destroy({
+      where: {
+        id: ids
+      }
+    })
+    res.json({ message: 'Remove banners success !!' })
+  } catch (error) {
+    res.status(422).json(error.message)
   }
 }
 
 module.exports = {
   create,
-  index
+  index,
+  findOne,
+  update,
+  remove
 }
--- router/bannerRouter.js
+++ router/bannerRouter.js
@@ -1,8 +1,11 @@
 const express = require('express')
 const bannerRouter = express.Router()
-const { create, index } = require('../controller/bannerController')
+const { create, index, findOne, update, remove } = require('../controller/bannerController')
 
 bannerRouter.post('/banners/create', create)
 bannerRouter.get('/banners', index)
+bannerRouter.get('/banners/:id', findOne)
+bannerRouter.patch('/banners/:id', update)
+bannerRouter.delete('/banners', remove)
 
 module.exports = bannerRouter
